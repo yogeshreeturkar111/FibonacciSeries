d0f8377cdde61d100b0f0692d56691cc0e44781d
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-03 22:54:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;  
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Text;  
+using System.Threading.Tasks;  
+  
+namespace FibonacciExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("---------------------Fibonacci Example--------------------");
+            Console.WriteLine("-----------------------------------------------------------");
+
+            int num1 = 0, num2 = 1, num3, num4;
+            Console.Write("Enter Number Of Element : ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your Fibonacci Series Of " + num3 + " Elements is below");
+            Console.Write(num1 + " " + num2 + " ");
+            for (int i = 2; i < num3; i++)
+            {
+                num4 = num1 + num2;
+                Console.Write(num4 + " ");
+                num1 = num2;
+                num2 = num4;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
